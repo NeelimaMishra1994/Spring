e13a53a3c4314361202f7b0c03ad1d3e6e55349f
neelima <neelimamishra172@gmail.com>
2023-12-17 19:55:07 +0000

    basic Hello world

---
+++ src/main/java/com/springCore/App.java
@@ -0,0 +1,18 @@
+package com.springCore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student1 = (Student) context.getBean("std");
+        Student student2 = (Student) context.getBean("std1");
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
